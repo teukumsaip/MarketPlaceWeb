5c12176ad4a80781e6822c7b94d6b984435b9d32
Saip-kun <saifsyafii22@gmail.com>
2023-05-01 14:25:46 +0700

    Belajar bikin API Register

---
+++ app/Models/Alamat.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Alamat extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'idUser',
+        'alamat',
+        'kecamatan',
+        'kota',
+
+    ];
+
+}
